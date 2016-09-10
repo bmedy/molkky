0082376562f96714edeccd24bd86efb1a1075dfa
Medy BELMOKHTAR <mbel@vif.fr>
2016-09-10 11:20:26 +0200

    clean

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,23 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import { BluetoothSerial } from 'ionic-native';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+  state: number;
+  serial: any;
   constructor(private navCtrl: NavController) {
-  
+    this.serial = BluetoothSerial.list();
+    this.state = 0;
   }
+
+  toggleLED(){
+    //
+    this.state = this.state == 0 ? 1 : 0;
+    //this.serial.write(this.state);
+    console.log('toggleLED : '+this.state);
+  }
+
 }
