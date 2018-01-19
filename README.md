3c56f845892459aff9762b872e3296b895262ce9
medy Belmokhtar <medy.belmoktar@gmail.com>
2018-01-19 21:48:39 +0100

    WIP

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ ionic serve
 ionic build ios
 ionic run ios --device
 
+
+## run on ios
+```
+ionic platform add ios
+ionic plugin add cordova-plugin-ble-central
+```
+
+open xcode add signing
+resolve warning
+close xcode
+
+```
+ionic run ios --device
+```
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,27 +1,77 @@
 import {Page, NavController} from 'ionic-angular';
-import { BluetoothSerial } from 'ionic-native';
+import { BLE } from 'ionic-native';
 import {UsersPage} from '../users/users';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  state: string;
-  availableDevice: any;
+
+  logs: Array<any>;
+  devices: Array<any>;
+  characteristics: Array<any>;
+  isScanning: boolean;
+  connecting: boolean;
+  connected: boolean;
+
   constructor(private navCtrl: NavController) {
-    
-    BluetoothSerial.list().then(a => {
-      console.log('devices : ',a);
-      this.availableDevice = a;
+    this.logs = ["start"];
+    this.devices = [];
+    this.isScanning = false;
+    this.connecting = false;
+    this.connected = false;
+
+    BLE.isEnabled().then(()=>{
+      console.error("BLE is enabled");
+      this.logs.push("BLE is enabled");
+      this.startScanning();
+    },(e)=>{
+      console.error(e);
+      this.logs.push(e);
     });
   }
-  
-  send(){
-    console.log(this.state);
+
+  users() {
+    this.navCtrl.push(UsersPage);
   }
 
-  users(){
-      this.navCtrl.push(UsersPage);
+  startScanning() {
+    console.log("Scanning Started");
+    this.logs.push("Scanning Started");
+    this.devices = [];
+    this.isScanning = true;
+   
+    BLE.startScan([]).subscribe(device => {
+      this.devices.push(device);
+    });
+  
+    setTimeout(() => {
+      BLE.stopScan().then(() => {
+        console.log("Scanning has stopped");
+        this.logs.push("Scanning has stopped");
+        console.log(JSON.stringify(this.devices))
+        this.logs.push(JSON.stringify(this.devices))
+        this.isScanning = false;
+      });
+    }, 3000);
+
+  }
+
+  connect(deviceID) {
+    this.characteristics = [];
+
+    BLE.connect(deviceID).subscribe(peripheralData => {
+      console.log(peripheralData.characteristics);
+      this.logs.push(peripheralData.characteristics);
+      this.characteristics = peripheralData.characteristics;
+      this.connecting = false;
+      this.connected = true;
+    },
+    peripheralData => {
+      console.log("disconnected");
+      this.logs.push("disconnected");
+      this.connected = false;
+    });
   }
 
 }
